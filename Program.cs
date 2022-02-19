e6b2f673ba48505bf7c11f599b664bd26882af3e
Matthew Sievers <MatterSievers@gmail.com>
2022-02-18 21:26:15 -0600

    added Employee class and refactored functions for best practice

--- Program.cs
+++ Program.cs
@@ -7,31 +7,41 @@ namespace CatWorx.BadgeMaker
   {
     static void Main(string[] args)
 {
-      List<string> employees = GetEmployees();
+      List<Employee> employees = GetEmployees();
       PrintEmployees(employees);
     }
 
-    static List<string> GetEmployees()
+    static List<Employee> GetEmployees()
     {
-      List<string> employees = new List<string>();
+      List<Employee> employees = new List<Employee>();
       while(true)
       {
-        Console.WriteLine("Please enter a name: (leave empty to exit): ");
-        string  input = Console.ReadLine();
-        if( input == "")
+        Console.WriteLine("Please Enter A First Name: (leave empty to exit): ");
+        string  firstName = Console.ReadLine();
+        if( firstName == "")
         {
           break;
         }
-        employees.Add(input);
+
+        Console.WriteLine("Please Enter Last Name: ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine("Please Enter Employee ID: ");
+        int id = Int32.Parse(Console.ReadLine());
+        Console.WriteLine("Please Enter Photo URL: ");
+        string photoUrl = Console.ReadLine();
+        //create a new Employee instance with inputs
+        Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+        employees.Add(currentEmployee);
       }
       return employees;
     }
 
-    static void PrintEmployees(List<string> employees)
+    static void PrintEmployees(List<Employee> employees)
     {
       for( int i = 0; i < employees.Count; i++)
       {
-        Console.WriteLine(employees[i]);
+        string template = "{0,-10}\t{1,-20}\t{2}";
+        Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetName(), employees[i].GetPhotoUrl()));
       }
     }
   }
